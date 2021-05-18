f80ed4839718fad15b583c43fffb63228d58425c
Yashas Shah <79524299+coolcoder-js@users.noreply.github.com>
2021-05-17 21:15:53 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const fs = require(`fs`);
+
+
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root: __dirname + "/public"})
+});
+
+app.get(`/img/:img`, (req, res) => {
+	const imgName = req.params.img;
+	if (!fs.existsSync(`./img/${imgName}`)) return res.end(400, `Invalid Image`);
+	else return res.sendFile(`${imgName}`, {root: __dirname + `/public/img`});
+})
+
+app.listen(8080, () => console.log(`Server listening at 8080!`));
